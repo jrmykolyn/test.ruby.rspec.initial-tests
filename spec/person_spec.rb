487967e53e3a51492de22f09ec1f0d936aa73c34
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-15 22:51:36 -0400

    Defined additional tests for 'initialize' and 'greet' methods on 'Person' class.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,8 +1,6 @@
 # Files within `lib/` can be included without providing additional path info.
 require 'person'
 
-
-
 RSpec.describe Person do
     # `context` method is used to group tests (eg. by method).
     # `context` is optional, `it` methods can be defined directly within the `describe ... do`.
@@ -12,6 +10,20 @@ RSpec.describe Person do
             me = Person.new( {} )
 
             expect( me.name ).to eq "John Doe"
+        end
+
+        it "Accepts a `:name` key via the `options` hash." do
+            me = Person.new( { :name => "Jesse" } )
+
+            expect( me.name ).to eq "Jesse"
+        end
+    end
+
+    context "#greet" do
+        it "Should return the correct greeting." do
+            me = Person.new( { :name => "Jesse" } )
+
+            expect( me.greet ).to eq "Hello, my name is Jesse"
         end
     end
 end
