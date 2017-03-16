286659653bac30dc1ba33ab8684db7d70a27d17b
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-15 22:44:56 -0400

    Added descriptive comments to 'person_spec.rb'.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,13 +1,17 @@
+# Files within `lib/` can be included without providing additional path info.
 require 'person'
 
+
+
 RSpec.describe Person do
-    
+    # `context` method is used to group tests (eg. by method).
+    # `context` is optional, `it` methods can be defined directly within the `describe ... do`.
     context "#initialize" do
-        it "Returns an instance with `name` = 'John Doe'" do
+        # `it` methods define and label individual tests.
+        it "Returns an instance with `name` = 'John Doe'." do
             me = Person.new( {} )
 
             expect( me.name ).to eq "John Doe"
         end
     end
-    
 end
